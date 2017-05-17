ea10e8a6d4178a054ae0a89ce772ed971ecac154
pythianali <ali@pythian.com>
2017-05-17 01:02:32 -0400

    Initial Commit

--- custom/profiles/manifests/puppetmasterprofile.pp
+++ custom/profiles/manifests/puppetmasterprofile.pp
@@ -33,7 +33,7 @@ class profiles::puppetmasterprofile {
     ensure => present,
     type   => 'ssh-rsa',
     target => '/root/.ssh/known_hosts',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFpXCnfblBvuMZxp8ZL9xTzBbiPl3POy9NiA3kU7w+ZnqUowoKjjKQNV6ePRssWiDX9ORQbN+PZ4pehrHlA1GZib2VOqgtDv7XXp2m1TD/d9cBKJDMkw64OpBdPB6OokVCsPvBC/c48yQfdq8Q5GxLXzX3YyqPMOa4J+crgwdNsUTEcIFUewKXGQ3uU4TAK+KqSLwlIoc2mjKH4H4o2l4+mUxWC2vFJjO/e8Hn3jDuqkh4pbT3paMjt7JsxtNJiP23YM+i4F5frVm80imuuKlpF+nl+onNRhBIVNoUoW0LRJnFPdZzc7MjsVwXiAF8nhtX8dALYoaRDY05FJEpxD83'
+    key => 'key here'
   }
 
 }
